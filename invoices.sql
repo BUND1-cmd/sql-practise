e40bc9a2fd00c09f29a9147ac0c123b5b68c767d
BUND1-CMD <bundipeetah@gmail.com>
2026-04-08 17:02:48 +0300

    some sql practise

--- invoices.sql
+++ invoices.sql
@@ -9,3 +9,9 @@ RIGHT JOIN vendors on invoices.vendor_name=vendors.vendor_name
 UNION
 select invoices.vendor_name,amount,country from invoices
 left join vendors on invoices.vendor_name=vendors.vendor_name
+
+select vendor_name,sum(amount) as total_amount,count(*) as invoice_count from invoices
+where status in ("Paid")
+group by vendor_name 
+having sum(amount)>300000
+order by total_amount desc
