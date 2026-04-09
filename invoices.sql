6671dbb512c3433bfc9ee1a44caff26369f6e1a3
BUND1-CMD <bundipeetah@gmail.com>
2026-04-09 10:20:48 +0300

    filter by departmant,inner join

--- invoices.sql
+++ invoices.sql
@@ -15,3 +15,13 @@ where status in ("Paid")
 group by vendor_name 
 having sum(amount)>300000
 order by total_amount desc
+
+select department,avg(amount) as invoice_amount,sum(amount)as total_spend,count(*)as invoice_count from invoices
+group by department
+having avg(amount)>200000
+order by total_spend desc
+
+select invoices.vendor_name,vendors.country,amount,status,vendors.vendor_type from invoices
+inner join vendors on invoices.vendor_name=vendors.vendor_name
+where country ="Kenya" and status in ("Pending","Overdue")
+order by amount DESC
